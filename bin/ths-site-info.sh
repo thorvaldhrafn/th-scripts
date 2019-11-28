5a9c95731682051293d867affaad879da420615f
thorvald <thorvald@gridinsoft.com>
2019-11-28 18:01:57 +0200

    Rejected commit: Default

--- bin/ths-site-info.sh
+++ bin/ths-site-info.sh
@@ -11,30 +11,32 @@ if [[ -z $1 ]]
         exit 0
 fi
 
-domain_fi=$1
+#domain_fi=$1
 
 nginx_conf_paths /etc/nginx/nginx.conf
 
-for n in ${CONF_LIST}
-	do
-		domain_list=`egrep "^[[:blank:]]*server_name.+" ${n} | grep -v localhost | awk '{ print $1=""; print $0 }' | sed 's/;//'`
-		echo ${domain_list} | egrep "[[:blank:]]*${domain_fi}[[:blank:]]" >/dev/null
-		if [[ $? -eq 0 ]]
-            then
-                root_path=`egrep "^[[:blank:]]*root[[:blank:]]*.+" ${n} | awk '{ print $2 }' | sed 's/;//'`
-                if [[ -n ${root_path} ]]
-                    then
-                        conf_path=${n}
-                        break
-                fi
-        fi
-    done
+echo ${CONF_LIST}
 
-site_user=`stat -c "%U" ${root_path}`
-
-if [[ -e ${root_path}/wp-config.php ]]
-    then
-        site_dbs=`cat ${root_path}/wp-config.php | grep DB_NAME | awk '{ print $2 }' | awk -F"'" '{ print $2 }'`
-fi
-
-echo "site_user=${site_user} root_path=${root_path} site_dbs=${site_dbs}"
+#for n in ${CONF_LIST}
+#	do
+#		domain_list=`egrep "^[[:blank:]]*server_name.+" ${n} | grep -v localhost | awk '{ print $1=""; print $0 }' | sed 's/;//'`
+#		echo ${domain_list} | egrep "[[:blank:]]*${domain_fi}[[:blank:]]" >/dev/null
+#		if [[ $? -eq 0 ]]
+#            then
+#                root_path=`egrep "^[[:blank:]]*root[[:blank:]]*.+" ${n} | awk '{ print $2 }' | sed 's/;//'`
+#                if [[ -n ${root_path} ]]
+#                    then
+#                        conf_path=${n}
+#                        break
+#                fi
+#        fi
+#    done
+#
+#site_user=`stat -c "%U" ${root_path}`
+#
+#if [[ -e ${root_path}/wp-config.php ]]
+#    then
+#        site_dbs=`cat ${root_path}/wp-config.php | grep DB_NAME | awk '{ print $2 }' | awk -F"'" '{ print $2 }'`
+#fi
+#
+#echo "site_user=${site_user} root_path=${root_path} site_dbs=${site_dbs}"
