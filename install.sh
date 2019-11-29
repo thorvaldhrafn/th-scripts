828150fba007e8567eb51ada191dc5b25feea456
thorvald <thorvald@gridinsoft.com>
2019-11-29 13:12:03 +0200

    Test commits

--- install.sh
+++ install.sh
@@ -15,9 +15,13 @@ fi
 cp confs/th-api.service /usr/lib/systemd/system/
 
 cp requirements.txt /usr/local/thscripts/
-su - thscripts -c "virtualenv --no-site-packages /usr/local/thscripts/.venv/"
-su - thscripts -c "source /usr/local/thscripts/.venv/bin/activate"
-su - thscripts -c "pip install -r requirements.txt"
+su thscripts
+cd ~
+virtualenv --no-site-packages /usr/local/thscripts/.venv/
+source /usr/local/thscripts/.venv/bin/activate
+pip install -r requirements.txt
+deactivate
+logout
 rm /usr/local/thscripts/requirements.txt
 
 systemctl daemon-reload
