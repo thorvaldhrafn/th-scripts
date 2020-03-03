bbde6cc71c0da3a0988d26b484a86d62c39264c2
thorvald <thorvald@gridinsoft.com>
2020-03-03 13:59:08 +0200

    Test commits

--- bin/ths-proc-info.py
+++ bin/ths-proc-info.py
@@ -22,6 +22,18 @@ import sys
 #         pool_conf_list.append(i)
 #
 
+
+def p_mem_rss_full(pname, p_mem_rss):
+    global proc_mem_list
+    try:
+        p_mem_rss_old = proc_mem_list[pname]['rss']
+        p_mem_rss_new = p_mem_rss_old + p_mem_rss
+        proc_mem_list[pname]['rss'] = p_mem_rss_new
+    except KeyError:
+        proc_mem_list[pname]['rss'] = p_mem_rss
+
+proc_mem_list = {}
+
 for prinfo in psutil.process_iter():
     try:
         cmd_first = prinfo.cmdline()[0]
@@ -29,9 +41,9 @@ for prinfo in psutil.process_iter():
             p_data_list = []
             pool = cmd_first.split()[-1]
             p_mem_data = prinfo.memory_info()
-            print(p_mem_data.rss)
-            for i in p_mem_data:
-                print(i)
-                print(type(i))
+            p_mem_rss = p_mem_data.rss
     except (psutil.NoSuchProcess, psutil.AccessDenied, IndexError):
         pass
+
+for key in proc_mem_list.keys():
+    print(key, proc_mem_list[key]['rss'])
