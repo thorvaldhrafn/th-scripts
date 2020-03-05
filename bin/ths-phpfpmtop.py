f7a9b4d675e1f058f5da4ea672d90ec4de49a3f9
thorvald <thorvald@gridinsoft.com>
2020-03-05 15:32:34 +0200

    Test commits

--- bin/ths-phpfpmtop.py
+++ bin/ths-phpfpmtop.py
@@ -1,26 +1,40 @@
+import curses
 import psutil
 import re
-import os
-import sys
-import curses
-import atexit
 
-def p_mem_rss_full(pname, p_mem_rss):
-    global proc_mem_list
-    try:
-        p_mem_rss_old = proc_mem_list[pname]['rss']
-        p_mem_rss_new = p_mem_rss_old + p_mem_rss
-        proc_mem_list[pname]['rss'] = p_mem_rss_new
-    except KeyError:
-        rss_dict = dict(rss=p_mem_rss)
+class FullPMemInfo(object):
+    def __init__(self):
+        self.proc_mem_list = {}
+
+    def p_mem_rss_full(self, pname, p_mem_rss):
         try:
-            p_dict = proc_mem_list[pname]
-            p_dict = {**p_dict, **rss_dict}
-            proc_mem_list[pname] = p_dict
+            p_mem_rss_old = self.proc_mem_list[pname]['rss']
+            p_mem_rss_new = p_mem_rss_old + p_mem_rss
+            self.proc_mem_list[pname]['rss'] = p_mem_rss_new
         except KeyError:
-            proc_mem_list[pname] = rss_dict
+            rss_dict = dict(rss=p_mem_rss)
+            try:
+                p_dict = self.proc_mem_list[pname]
+                p_dict = {**p_dict, **rss_dict}
+                self.proc_mem_list[pname] = p_dict
+            except KeyError:
+                self.proc_mem_list[pname] = rss_dict
 
-proc_mem_list = {}
+    def p_mem_vms_full(self, pname, p_mem_vms):
+        try:
+            p_mem_vms_old = self.proc_mem_list[pname]['vms']
+            p_mem_vms_new = p_mem_vms_old + p_mem_vms
+            self.proc_mem_list[pname]['rss'] = p_mem_vms_new
+        except KeyError:
+            vms_dict = dict(rss=p_mem_vms)
+            try:
+                p_dict = self.proc_mem_list[pname]
+                p_dict = {**p_dict, **vms_dict}
+                self.proc_mem_list[pname] = p_dict
+            except KeyError:
+                self.proc_mem_list[pname] = vms_dict
+
+FullPMemInfo = FullPMemInfo()
 
 for prinfo in psutil.process_iter():
     try:
@@ -30,9 +44,12 @@ for prinfo in psutil.process_iter():
             pool = cmd_first.split()[-1]
             p_mem_data = prinfo.memory_info()
             p_mem_rss = p_mem_data.rss
-            p_mem_rss_full(pool, p_mem_rss)
+            FullPMemInfo.p_mem_rss_full(pool, p_mem_rss)
     except (psutil.NoSuchProcess, psutil.AccessDenied, IndexError):
         pass
 
-for key in proc_mem_list.keys():
-    print(key, proc_mem_list[key]['rss'])
+print(FullPMemInfo.proc_mem_list)
+#
+# for key in proc_mem_list.keys():
+#     print(key, proc_mem_list[key]['rss'])
+
