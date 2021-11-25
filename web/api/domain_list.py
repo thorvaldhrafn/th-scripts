216d16e31322927c08375571b95ae85c6ba427b4
thorvald <thorvald@gridinsoft.com>
2021-11-25 13:21:28 +0200

    debug

--- web/api/domain_list.py
+++ web/api/domain_list.py
@@ -79,9 +79,7 @@ def check_vhost(host_list, param):
         try:
             for rdata in google_resolver.query(vhost, "A"):
                 host_ip = str(rdata)
-                print(vhost, host_ip)
-                print(ip_list)
-                if ip_list.count(host_ip):
+                if host_ip in ip_list:
                     domains[vhost] = host_ip
                 else:
                     wrong_domains[vhost] = host_ip
