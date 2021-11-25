38ced52e9e9061b773049b0f72dcb3aa1e9335c9
thorvald <thorvald@gridinsoft.com>
2021-11-25 15:42:39 +0200

    debug

--- install.sh
+++ install.sh
@@ -13,21 +13,21 @@ if [[ $(virtualenv  --version >/dev/null 2>&1; echo $?) -ne 0 ]]; then
   exit 1
 fi
 
-#mkdir /usr/local/thscripts/ 2>/dev/null
-#useradd -s /bin/bash -d /usr/local/thscripts/ -m thscripts
-#
-#if [[ $install_param == "install" ]]; then
-#  if [[ -d /etc/nginx/ ]]; then
-#    cp confs/nginx_thscripts.conf /etc/nginx/conf.d/thscripts.conf
-#  fi
-#fi
-#
+mkdir /usr/local/thscripts/ 2>/dev/null
+useradd -s /bin/bash -d /usr/local/thscripts/ -m thscripts
+
+if [[ $install_param == "install" ]]; then
+  if [[ -d /etc/nginx/ ]]; then
+    cp confs/nginx_thscripts.conf /etc/nginx/conf.d/thscripts.conf
+  fi
+fi
+
 dir_list="bin etc web"
-#
-#for i in $dir_list; do
-#  mkdir /usr/local/thscripts/"${i}" 2>/dev/null
-#  rsync -aq --delete --exclude=/usr/local/thscripts/etc/global.config "${i}"/ /usr/local/thscripts/"${i}"/
-#done
+
+for i in $dir_list; do
+  mkdir /usr/local/thscripts/"${i}" 2>/dev/null
+  rsync -aq --delete --exclude=/usr/local/thscripts/etc/global.config "${i}"/ /usr/local/thscripts/"${i}"/
+done
 
 dir_list_full="$dir_list .venv"
 
@@ -44,40 +44,40 @@ find /usr/local/thscripts/ -maxdepth 1 -mindepth 1 -type d | while read -r line;
   fi
 done
 
-#if [[ ! -d /usr/local/thscripts/.venv/ ]]; then
-#    update_param="venv_update"
-#fi
-#
-#if [[ $install_param == "install" || $update_param == "venv_update" ]]; then
-#  cp confs/th-api.service /usr/lib/systemd/system/
-#  chown -R thscripts:thscripts /usr/local/thscripts/
-#  VIRT_ENV="/usr/local/thscripts/.venv/"; sed -i "s|VIRT_ENV|${VIRT_ENV}|g" /usr/lib/systemd/system/th-api.service
-#  sed -i "s|SYS_PATH|${PATH}|g" /usr/lib/systemd/system/th-api.service
-#
-#  cp requirements.txt /usr/local/thscripts/
-#  su - thscripts -c "\
-#  virtualenv --no-site-packages /usr/local/thscripts/.venv/ && \
-#  source /usr/local/thscripts/.venv/bin/activate && \
-#  pip install -r requirements.txt && \
-#  deactivate"
-#  rm /usr/local/thscripts/requirements.txt
-#
-#  systemctl daemon-reload
-#  systemctl enable th-api.service
-#  systemctl start th-api.service
-#fi
-#
-#if [[ $install_param == "update" && $update_param != "venv_update" ]]; then
-#  cp requirements.txt /usr/local/thscripts/
-#  su - thscripts -c "\
-#  source /usr/local/thscripts/.venv/bin/activate && \
-#  pip install -r requirements.txt && \
-#  deactivate"
-#  rm /usr/local/thscripts/requirements.txt
-#fi
-#
-#systemctl is-active --quiet th-api.service && systemctl restart th-api.service
-#
-#if [[ -d /etc/nginx/ ]]; then
-#    systemctl is-active --quiet nginx && systemctl reload nginx
-#fi
+if [[ ! -d /usr/local/thscripts/.venv/ ]]; then
+    update_param="venv_update"
+fi
+
+if [[ $install_param == "install" || $update_param == "venv_update" ]]; then
+  cp confs/th-api.service /usr/lib/systemd/system/
+  chown -R thscripts:thscripts /usr/local/thscripts/
+  VIRT_ENV="/usr/local/thscripts/.venv/"; sed -i "s|VIRT_ENV|${VIRT_ENV}|g" /usr/lib/systemd/system/th-api.service
+  sed -i "s|SYS_PATH|${PATH}|g" /usr/lib/systemd/system/th-api.service
+
+  cp requirements.txt /usr/local/thscripts/
+  su - thscripts -c "\
+  virtualenv --no-site-packages /usr/local/thscripts/.venv/ && \
+  source /usr/local/thscripts/.venv/bin/activate && \
+  pip install -r requirements.txt && \
+  deactivate"
+  rm /usr/local/thscripts/requirements.txt
+
+  systemctl daemon-reload
+  systemctl enable th-api.service
+  systemctl start th-api.service
+fi
+
+if [[ $install_param == "update" && $update_param != "venv_update" ]]; then
+  cp requirements.txt /usr/local/thscripts/
+  su - thscripts -c "\
+  source /usr/local/thscripts/.venv/bin/activate && \
+  pip install -r requirements.txt && \
+  deactivate"
+  rm /usr/local/thscripts/requirements.txt
+fi
+
+systemctl is-active --quiet th-api.service && systemctl restart th-api.service
+
+if [[ -d /etc/nginx/ ]]; then
+    systemctl is-active --quiet nginx && systemctl reload nginx
+fi
